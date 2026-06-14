b70f7eeabe8c01140dd42e3b339d9f268bcd1d8b
Lucas Sousa <156027958+lucasdevii@users.noreply.github.com>
2026-06-14 18:58:29 +0000

    feat: Inicialização básica do projeto vue@latest

---
+++ UIGenerator/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const canvas = ref(null)
+
+onMounted(() => {
+  const ctx = canvas.value.getContext('2d')
+
+})
+</script>
+
+<template>
+  <main class="container">
+    <div class="render">
+      <button>Renderizar</button>
+      <canvas
+        ref="canvas"
+        width="600"
+        height="400"
+      ></canvas>
+    </div>
+  </main>
+
+</template>
+
+<style scoped>
+.render{
+  
+}
+</style>
