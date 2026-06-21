76cacb50fd3d4bc3d63d101abfaed054a7cb7dbe
Lucas <lucassousa180810@gmail.com>
2026-06-21 03:51:03 -0300

    feat: implementação inicial do algoritmo Wilson

--- UIGenerator/src/utils/algorithms/pg_simple/wilson.js
+++ UIGenerator/src/utils/algorithms/pg_simple/wilson.js
@@ -1,37 +1,84 @@
+import Grid from "../../services/grid"
+
 const getAllCellCoordinates = (scale) = (scale) => {
-    const cells = []
+    const cells = new Set()
     for(let row = 0; row < scale; row++){
         for(let col = 0; col < scale; col++){
-            cells.push({row, col})
+            cells.add(`${row},${col}`) // "(Posição em X),(Posição em Y)"
         }
     }
     return cells
 }
 
+/**
+ * Remove as celulas do topo, até chegar na celula do path atual encostada 
+ * 
+ * @param {Array<{x: col, y: row}>} currentPath 
+ * @param {{x: col, y: row}} next 
+ */
+const removeCellsOfTop = (currentPath, next) => {
+    while (currentPath.length > 0 && currentPath[currentPath.length - 1] !== next) {
+        currentPath.pop()
+    }
+}
+
+/**
+ * Move uma célula do conjunto de não visitadas para o conjunto de visitadas.
+ *
+ * @param {Object} params
+ * @param {Set<string>} params.visitedCells
+ * @param {Set<string>} params.unvisitedCells
+ * @param {Array<{x: number, y: number}>} params.currentPath
+ */
+const setVisitedAndUnvisitedCells = ({visitedCells, unvisitedCells, currentPath}) => {
+    for(let i = 0; i < currentPath.length; i++){
+        unvisitedCells.remove(`${currentPath[i].x},${currentPath[i].y}`)
+        visitedCells.add(`${currentPath[i].x},${currentPath[i].y}`)
+    }
+}
+
+/**
+ * Cria um labirinto utilizando o algoritmo Wilson.
+ *
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {Grid} matrix
+ */
 const createMaze = (ctx, matrix) => {     
     const unvisitedCells = getAllCellCoordinates(matrix.length);
-    const visitedCells = [];
+    const visitedCells = new Set();
     
-    while(unvisitedCells.length > 0){
-        let findVisitedCell = false
-        const arrivalIndex = matrix.getRandomIndex()
-        const exitIndex = matrix.getRandomIndex()
-        while(!findVisitedCell){
-            const percent = Math.random()
+    //O loop que continua enquanto tiver celulas ainda sem conexões
+    while(unvisitedCells.size > 0){
+        const currentPath = [];  //Array<{x: col, y: row}>
 
-            if(percent < 0.25){
-                
-            }else if(percent < 0.50){
+        const initIndex = matrix.getRandomIndex()   //{x: col, y: row}
+        const targetIndex = matrix.getRandomIndex()   //{x: col, y: row}
 
-            }else if(percent < 0.75){
+        let currentIndex = initIndex
 
-            }else{
-
+        //Loop que cria os novos caminhos
+        while(currentIndex !== targetIndex){
+            const neighbors = matrix.getValidNeighbors(currentIndex.x,currentIndex.y)  //Array<{x: col, y: row}>
+            const indexOfNext = Math.floor(Math.random() * neighbors.length)
+            //Selecion um vizinho aleatorio a partir de seu index, no array neighbors
+            const next = neighbors[indexOfNext]   //{x: col, y: row}
+            
+            //Se tiver, remove todas as celulas já andadas a partir desse ponto de contato, pois não pode haver pontos de conexão em um mesmo path
+            if(currentPath.includes(next)){
+                removeCellsOfTop(currentPath, next)
+                continue
             }
+            else if(visitedCells.has(`${next.x},${next.y}`)){
+                currentIndex = next
+                continue
+            }
+            currentPath.push(currentIndex)
+            currentIndex = next
         }
+
+
+        setVisitedAndUnvisitedCells({visitedCells, unvisitedCells, currentPath})
     }     
-    const randomPosition = matrix.getRandomIndex()
-    
 
     return 
 }
