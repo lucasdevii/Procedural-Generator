c4a3885b54462d7f96a1908fecb921b77639900d
Lucas Sousa <156027958+lucasdevii@users.noreply.github.com>
2026-06-23 13:47:53 +0000

    feat: adicionada função de retirar e adicionar items dos sets de visitamento. Iniciada função de iniciar o valor inicial das casas ainda não visitadas

--- UIGenerator/src/utils/algorithms/pg_simple/wilson.js
+++ UIGenerator/src/utils/algorithms/pg_simple/wilson.js
@@ -1,14 +1,30 @@
 const sameCell = (a,b) => a.x == b.x && a.y == b.y 
 
-const addInfosInSetList = () => {
-    
+const setVisitedAndUnvisitedCells = (visitedCells, unvisitedCells, currentPath) => {
+    for(path of currentPath){
+        visitedCells.add(`${currentPath.x},${currentPath.y}`)
+        unvisitedCells.remove(`${currentPath.x},${currentPath.y}`)
+    }
+}
+
+/**
+ * Inicializa os valores no Set das celulas ainda não visitadas com as coordenadas de todas as celulas da matrix
+ * 
+ * @param {Set<{x,y}>} unvisitedCells 
+ * @param {Array<Cell>} matrix 
+ */
+const initUnvisitedCells = (unvisitedCells, matrix) => {
+    for(let row = 0; row < matrix.length; row++){
+        for(let col = 0; col < matrix.length; col++){
+            unvisitedCells.add(`${col},${row}`)
+        }
+    }
+
 }
 
 const currentPathGenerator = ({visitedCells, unvisitedCells, matrix}) => {
     const currentPath = []
     
-    const currentIndex = null
-
     while(visitedCells.has(`${currentIndex.x},${currentIndex.y}`)){
         
     }
@@ -21,10 +37,15 @@ const mazeGeneratorData = (matrix) => {
     const visitedCells = new Set()
     const unvisitedCells = new Set()
 
+    initUnvisitedCells(unvisitedCells, matrix)
 
     while(!visitedCells.size){
         const currentPath = currentPathGenerator({visitedCells, unvisitedCells, matrix})
+        
+        setVisitedAndUnvisitedCells(visitedCells, unvisitedCells, currentPath)
+        matrix.setInTheGrid(currentPath)
     }
+
 }
 
 export default mazeGeneratorData
